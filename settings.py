1a484b1055c4b1c2647f77a38de4235c9d4b44ff
Antonio Ossa <aaossa@uc.cl>
2016-07-24 16:19:10 -0400

    Fix envars naming

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from os import environ
 
 TELEGRAM_API_URL = "https://api.telegram.org/bot{}/{}"
 TELEGRAM_BOT_NAME = environ["TELEGRAM_BOT_NAME"]
-TELEGRAM_SECRET_URL = environ["TELEGRAM_URL"]
-TELEGRAM_URL = environ["TELEGRAM_URL"]
+TELEGRAM_SECRET_URL = environ["TELEGRAM_SECRET_URL"]
+TELEGRAM_TOKEN = environ["TELEGRAM_TOKEN"]
