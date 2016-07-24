7a99049da31915f470d5d283475fef6c6176a6b7
Antonio Ossa <aaossa@uc.cl>
2016-07-24 14:59:35 -0400

    Constants module

---
+++ settings.py
@@ -0,0 +1,6 @@
+from os import environ
+
+TELEGRAM_API_URL = "https://api.telegram.org/bot{}/{}"
+TELEGRAM_BOT_NAME = environ["TELEGRAM_BOT_NAME"]
+TELEGRAM_SECRET_URL = environ["TELEGRAM_URL"]
+TELEGRAM_URL = environ["TELEGRAM_URL"]
